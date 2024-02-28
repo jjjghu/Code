c0c1442fb6f92d9d5439735d9677522a632130d3
jjjghu <chiuliyou@gmail.com>
2024-02-28 11:36:58 +0800

    Delete

--- 專題/DJango/FIRST/Scripts/p/p/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-urlpatterns =[
-    path('admin/', admin.site.urls),
-    path('',include('a.urls')),
-    # p 接上 app.urls 當中的內容
-]
