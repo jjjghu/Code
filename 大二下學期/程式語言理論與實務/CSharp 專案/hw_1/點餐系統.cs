4d9e4839df644a4d0f8b2b2583e064a90fa599b7
jjjghu <chiuliyou@gmail.com>
2024-04-15 13:24:44 +0800

    CSharp

---
+++ 大二下學期/程式語言理論與實務/CSharp 專案/hw_1/點餐系統.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace hw1
+{
+    public partial class 點餐系統 : Form
+    {
+        public 程式語言理論與實務 preForm;
+        public 點餐系統()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.preForm.preForm.Close();
+            this.preForm.Close();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.preForm.Show();
+            this.Close();
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked == true) { groupBox2.Enabled = true; }
+            else
+            {
+                groupBox2.Enabled = false;
+                radioButton1.Checked = false;
+                radioButton2.Checked = false; 
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked == true) { groupBox3.Enabled = true; }
+            else
+            {
+                groupBox3.Enabled = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+                radioButton5.Checked = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+        private void Cal()
+        {
+            int cost = 0;
+            if (checkBox1.Checked) { cost += 69; }
+            if (checkBox2.Checked) { cost += 49; }
+            if (checkBox3.Checked) { cost += 59; }
+            if (checkBox4.Checked) { cost += 79; }
+            if (radioButton1.Checked) { cost += 35; }
+            if (radioButton2.Checked) { cost += 25; }
+            if (radioButton3.Checked) { cost += 35; }
+            if (radioButton4.Checked) { cost += 25; }
+            if (radioButton5.Checked) { cost += 45; }
+            textBox1.Text = "" + cost;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            Cal();
+        }
+
+
+    }
+}
---
+++ 大二下學期/程式語言理論與實務/CSharp 專案/hw_2/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace hw_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 12F);
+            label1.Location = new Point(91, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 25);
+            label1.TabIndex = 0;
+            label1.Text = "學號";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 12F);
+            label2.Location = new Point(91, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 25);
+            label2.TabIndex = 0;
+            label2.Text = "姓名";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei UI", 12F);
+            label3.Location = new Point(91, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 25);
+            label3.TabIndex = 0;
+            label3.Text = "國文";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft JhengHei UI", 12F);
+            label4.Location = new Point(91, 176);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 25);
+            label4.TabIndex = 0;
+            label4.Text = "數學";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft JhengHei UI", 12F);
+            label5.Location = new Point(124, 245);
+            label5.Name = "label5";
+            label5.Size = new Size(157, 25);
+            label5.TabIndex = 0;
+            label5.Text = "共有         筆資料";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(198, 300);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "資料輸入";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(298, 300);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "學號搜尋";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(398, 300);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 1;
+            button3.Text = "資料顯示";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(315, 47);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(266, 166);
+            textBox1.TabIndex = 2;
+            textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(161, 65);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(131, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(161, 100);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(131, 27);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(161, 136);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(131, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(161, 174);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(131, 27);
+            textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(170, 243);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(39, 27);
+            textBox6.TabIndex = 3;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft JhengHei UI", 12F);
+            label6.Location = new Point(279, 245);
+            label6.Name = "label6";
+            label6.Size = new Size(92, 25);
+            label6.TabIndex = 0;
+            label6.Text = "國文平均";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft JhengHei UI", 12F);
+            label7.Location = new Point(432, 245);
+            label7.Name = "label7";
+            label7.Size = new Size(92, 25);
+            label7.TabIndex = 0;
+            label7.Text = "數學平均";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(367, 243);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(59, 27);
+            textBox7.TabIndex = 3;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(519, 243);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(62, 27);
+            textBox8.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox7;
+        private TextBox textBox8;
+    }
+}
