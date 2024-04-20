73044fc3ddcf961750f7d8bf075921df8c5c6b34
jjjghu <chiuliyou@gmail.com>
2024-04-20 19:16:47 +0800

    Update

---
+++ 雜項/設計模式/物件導向基礎知識/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 物件導向基礎知識
+{
+    internal class Cat:Animal
+    {
+        public Cat():base() // 建構子
+        { }
+        public Cat(string name) : base(name) // 方法重載建構子
+        { }
+        protected override string getShoutSound() // 多型
+        {
+            return " 喵";
+        }
+    }
+}
