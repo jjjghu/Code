0984e1413885efc0ede3570b4fd0582ee0ffef90
jjjghu <chiuliyou@gmail.com>
2024-12-18 22:24:08 +0800

    common_sequence

--- 大三上/演算法/作業/hw5 最大共同子整數問題/common_sequence.cpp
+++ 大三上/演算法/作業/hw5 最大共同子整數問題/common_sequence.cpp
@@ -1,3 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+bool is_greater(const string &s1, const string &s2)
+{
+    string s1_trim = s1, s2_trim = s2;
+    s1_trim.erase(0, s1_trim.find_first_not_of('0'));
+    s2_trim.erase(0, s2_trim.find_first_not_of('0'));
+    if (s1_trim.empty())
+        s1_trim = "0";
+    if (s2_trim.empty())
+        s2_trim = "0";
+    if (s1_trim.length() != s2_trim.length())
+        return s1_trim.length() > s2_trim.length();
+    return s1_trim > s2_trim;
+}
+void solve(char *fileName)
+{
+    ifstream inFile(fileName);
+    if (!inFile.is_open())
+    {
+        cout << "無法開啟檔案: " << fileName << "\n";
+        return;
+    }
+    string s1, s2;
+    while (inFile >> s1 >> s2)
+    {
+        int n = s1.length(), m = s2.length();
+        vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= m; ++j)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
+                }
+                else
+                {
+                    dp[i][j] = is_greater(dp[i - 1][j], dp[i][j - 1]) ? dp[i - 1][j] : dp[i][j - 1];
+                }
+            }
+        }
+        string result = dp[n][m];
+        result.erase(0, result.find_first_not_of('0'));
+        // cout << "The common subsequence of " << s1 << " and " << s2 << " is ";
+        cout << (result.empty() ? "0" : result) << endl;
+    }
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cout << "Usage: " << argv[0] << " <input_file_1> <input_file_2> ... <input_file_n>\n";
+        return 1;
+    }
+    for (int i = 1; i < argc; ++i)
+    {
+        solve(argv[i]);
+    }
+    return 0;
+}
+/*
+動態規劃五步法
+1. 構造問題: s1, s2 的最長共同子序列
+2. 定義狀態: f(i, j) = s1[:i] 和 s2[:j] 的最長共同子序列
+3. 求解小規模的簡單問題:
+   f(i, 0) = ""
+   f(0, j) = ""
+4. 狀態轉移方程式:
+    f(i, j) =
+    {
+        if s1[i] == s2[j] : f(i - 1, j - 1) + s1[i];
+        else: 選擇 f(i - 1, j), f(i, j - 1) 比較長的
+    }
+5. 判斷複雜度: O(n^2)
+*/
 /*
 最大共同子整數問題 (pdf可到公告區下載)
 給定一個正數 N1=2579413，
@@ -15,60 +98,21 @@
 單一輸入整數最大位數為50位。
 
 輸出說明： 每組測試案例輸出一行，輸出為一個整數。
-範例輸入1： 1357904 20468 範例輸出1：4
-範例輸入2： 2579413 354573 範例輸出2： 573
-範例輸入3： 12365402345 316524015331 範例輸出3： 365405
-範例輸入4： 123654045 31654015 範例輸出4： 365405
-範例輸入5： 10579413 015143 範例輸出5： 1543
+範例輸入1： 1357904 20468
+範例輸出1：4
+
+範例輸入2： 2579413 354573
+範例輸出2： 573
+
+範例輸入3： 12365402345 316524015331
+範例輸出3： 365405
+
+範例輸入4： 123654045 31654015
+範例輸出4： 365405
+
+範例輸入5： 10579413 015143
+範例輸出5： 1543
+
 範例輸入6： 123654045123654045 3165401531654015
 範例輸出6： 365405365405
-*/
-#include <iostream>
-#include <vector>
-using namespace std;
-long long to_number(string s)
-{
-    long long num = 0;
-    for (int i = 0; i < s.length(); ++i)
-    {
-        num = num * 10 + s[i] - '0';
-    }
-    return num;
-}
-int main(void)
-{
-    string s1, s2;
-    cin >> s1 >> s2;
-    int n = s1.length(), m = s2.length();
-    vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 1; j <= m; ++j)
-        {
-            if (s1[i - 1] == s2[j - 1])
-            {
-                dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
-            }
-            else
-            {
-                dp[i][j] = to_number(dp[i - 1][j]) > to_number(dp[i][j - 1]) ? dp[i - 1][j] : dp[i][j - 1];
-            }
-        }
-    }
-    cout << to_number(dp[n][m]);
-}
-/*
-動態規劃五步法
-1. 構造問題: s1, s2 的最長共同子序列
-2. 定義狀態: f(i, j) = s1[:i] 和 s2[:j] 的最長共同子序列
-3. 求解小規模的簡單問題:
-   f(i, 0) = ""
-   f(0, j) = ""
-4. 狀態轉移方程式:
-    f(i, j) =
-    {
-        if s1[i] == s2[j] : f(i - 1, j - 1) + s1[i];
-        else: 選擇 f(i - 1, j), f(i, j - 1) 比較長的
-    }
-5. 判斷複雜度: O(n^2)
 */
